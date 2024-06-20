bff26f42fd7c1d4e4843d1621d52a455dc9629c5
MD Fuad <fuadhasa199@gmail.com>
2024-06-20 07:50:09 +0600

    all model created and registered

--- appoitment/admin.py
+++ appoitment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from appoitment.models import Appointment
 # Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['Doctor_name','Patient_name','appointment_types','appointment_status','status','time']
+
+    def Patient_name(self,obj):
+        return obj.patient.user.first_name
+    
+    def Doctor_name(self,obj):
+        return obj.doctor.user.first_name
+    
+admin.site.register(Appointment,AppointmentAdmin)
+    
+    
